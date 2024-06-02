212bf03c2f2165a1f914730754a31aa0fa74f5a9
musa islam <hamerofth@gmail.com>
2024-06-01 21:45:59 -0400

    0

---
+++ MoveEstimator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+public class MoveEstimator
+{
+    static void Main(string[] args)
+    {
+        const int baserate = 200;
+        const int hourlyrate = 150;
+        const decimal mileyrate = 2;
+
+        Write("number of hours: ");
+        decimal hours = decimal.Parse(ReadLine());
+        Write("number of miles: ");
+        decimal miles = decimal.Parse(ReadLine());
+
+        decimal total = baserate + (hours * hourlyrate) + (miles * mileyrate);
+        WriteLine(total.ToString("C")); 
+
+    }
+
+}
